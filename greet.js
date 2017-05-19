7b22502990569e216a8f1967ad71821223aa169b
Yamkela <owamjames1@gmail.com>
2017-05-19 10:40:31 +0200

    my first back-end exercise

---
+++ greet.js
@@ -0,0 +1,9 @@
+function greet(neighboursName){
+var greetings="Hello, "+neighboursName;
+  console.log (greetings);
+  return greetings;
+};
+greet('Yamkela');
+
+var msg = greet('Yamkela')
+console.log(msg);
