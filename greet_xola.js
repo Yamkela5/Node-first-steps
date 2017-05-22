60ff622a9623ae1fcdbd1c5638678a26458a1def
Yamkela <owamjames1@gmail.com>
2017-05-22 12:56:59 +0200

    adding

--- greet_xola.js
+++ greet_xola.js
@@ -1,5 +1,5 @@
 const chalk = require('chalk');
 const greet = require('./greet');
 
-const styledMessage = chalk.bgGreen.black(greet('Athenkosi'));
+const styledMessage = chalk.bgGreen.black(greet('Kwanele'));
 console.log(styledMessage)
